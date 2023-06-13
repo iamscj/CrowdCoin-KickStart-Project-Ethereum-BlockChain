37a97ee5144f15b86c7611df538e630d65899e37
iamscj <91722818+iamscj@users.noreply.github.com>
2023-06-13 17:41:37 +0530

    complete

---
+++ .next/5.78f0317b3e45a7b8155a.hot-update.js
@@ -0,0 +1,10 @@
+webpackHotUpdate(5,{
+
+/***/ 1095:
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: Error: ENOENT: no such file or directory, open 'D:\\kickstart\\ethereum\\build\\CampaignFactory.json'");
+
+/***/ })
+
+})
